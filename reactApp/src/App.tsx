17cd52a8f20d050e51adbf166ade4c1a2ab8a92c
Abdullah Mahdi <134118222+Abdullah-Mahdi272@users.noreply.github.com>
2024-05-12 02:57:18 -0400

    Update App.tsx

--- reactApp/src/App.tsx
+++ reactApp/src/App.tsx
@@ -1,16 +1,36 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import AlertWithButton from "./components/AlertWithButton";
+import Button from "./components/Button";
+// function App() {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+//   //items = [];
+//   //const ifEmpty = items.length === 0 ? <p>No item found</p> : null;
+//   // const ifEmpty = () => {
+//   //   return items.length === 0 && <p>No item found</p>;
+//   // };
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       ></ListGroup>
+//     </div>
+//   );
+// }
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  //items = [];
-  //const ifEmpty = items.length === 0 ? <p>No item found</p> : null;
-  // const ifEmpty = () => {
-  //   return items.length === 0 && <p>No item found</p>;
-  // };
   return (
     <div>
-      <ListGroup items={items} heading="Cities"></ListGroup>
+      <Alert>My Alert</Alert>
+      <Button color="dark" functionality={() => {}}>
+        My Button
+      </Button>
     </div>
   );
 }

ed17702a1f5b788938a4ff73e42a4db66d1ae7c4
Abdullah Mahdi <134118222+Abdullah-Mahdi272@users.noreply.github.com>
2024-05-12 01:14:08 -0400

    Update App.tsx

--- reactApp/src/App.tsx
+++ reactApp/src/App.tsx
@@ -2,23 +2,16 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   //items = [];
   //const ifEmpty = items.length === 0 ? <p>No item found</p> : null;
-
-  const ifEmpty = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
-
+  // const ifEmpty = () => {
+  //   return items.length === 0 && <p>No item found</p>;
+  // };
   return (
-    <>
-      <h1>List</h1>
-      {ifEmpty()}
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <ListGroup items={items} heading="Cities"></ListGroup>
+    </div>
   );
 }
 
